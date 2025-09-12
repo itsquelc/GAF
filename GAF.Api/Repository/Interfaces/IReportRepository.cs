51449b30a47f150471781eef2039e544c7276d68
itsquelc <raquelnunescirino@gmail.com>
2025-09-12 10:45:06 -0300

    klkjllj

---
+++ GAF.Api/Repository/Interfaces/IReportRepository.cs
@@ -0,0 +1,10 @@
+using GAF.Api.Models;
+
+namespace GAF.Api.Repositories.Interfaces;
+
+public interface IReportRepository
+{
+    Task<Report> GetMonthlyReportAsync(string userId, int month, int year);
+    Task<IEnumerable<Report>> GetYearlyReportsAsync(string userId, int year);
+    Task<Report> CreateOrUpdateMonthlyReportAsync(Report report);
+}
